d250e785db7cff1853817af89a028aa14e726d07
Nathanael Burt <nathanael.burt@gmail.com>
2014-05-02 14:57:45 -0600

    changed if statement for index
    now checks that user exists in the database

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   require 'twitter'
 
   def index
-    if session[:user_id]
+    if current_user
       client = Twitter::REST::Client.new do |config|
         config.consumer_key = ENV['TWITTER_API_KEY']
         config.consumer_secret = ENV['TWITTER_API_SECRET']

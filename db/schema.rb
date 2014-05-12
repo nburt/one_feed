59d48ff4c93db43303074e190cfa34ef3301a3e4
Nathanael Burt <nathanael.burt@gmail.com>
2014-05-12 15:06:02 -0600

    added index for user id in tokens table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140512205036) do
+ActiveRecord::Schema.define(version: 20140512210342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 20140512205036) do
     t.string  "access_token_secret"
     t.integer "user_id"
   end
+
+  add_index "tokens", ["user_id"], name: "index_tokens_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.datetime "created_at"

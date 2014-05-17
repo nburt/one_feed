0393ce2d054196e9ccf3fe483958b9b2f96f084a
Nathanael Burt <nathanael.burt@gmail.com>
2014-05-17 12:17:40 -0600

    refactoring
    moved logic into private methods

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -10,32 +10,40 @@ class Feed
 
   def posts
     twitter_timeline = []
-
     if current_user_has_provider?('twitter')
-      token = @current_user.tokens.find_by(provider: 'twitter')
-      client = token.configure_twitter(token.access_token, token.access_token_secret)
-      begin
-        twitter_timeline = client.home_timeline
-      rescue Twitter::Error::Forbidden
-        report_twitter_error
-      rescue Twitter::Error::Unauthorized
-        report_twitter_error
-      end
+      twitter_timeline = get_twitter_timeline
     end
 
-    instagram_timeline = if current_user_has_provider?('instagram')
-                           token = @current_user.tokens.find_by(provider: 'instagram')
-                           instagram_api = InstagramApi.new(token.access_token)
-                           timeline = instagram_api.get_timeline
-                           timeline.posts
-                         else
-                           []
-                         end
+    instagram_timeline = []
+    if current_user_has_provider?('instagram')
+      instagram_timeline = get_instagram_timeline
+    end
 
-    TimelineConcatenator.new.merge(twitter_timeline, instagram_timeline)
+    timeline_concatenator = TimelineConcatenator.new
+    timeline_concatenator.merge(twitter_timeline, instagram_timeline)
   end
 
   private
+
+  def get_twitter_timeline
+    token = @current_user.tokens.find_by(provider: 'twitter')
+    client = token.configure_twitter(token.access_token, token.access_token_secret)
+    begin
+      twitter_timeline = client.home_timeline
+    rescue Twitter::Error::Forbidden
+      report_twitter_error
+    rescue Twitter::Error::Unauthorized
+      report_twitter_error
+    end
+    twitter_timeline
+  end
+
+  def get_instagram_timeline
+    token = @current_user.tokens.find_by(provider: 'instagram')
+    instagram_api = InstagramApi.new(token.access_token)
+    timeline = instagram_api.get_timeline
+    timeline.posts
+  end
 
   def current_user_has_provider?(provider)
     @current_user.tokens.by_name(provider).any?

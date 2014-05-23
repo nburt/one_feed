04e68220d6eeee968d89b055f1e8ed4c05578477
Nathanael Burt <nathanael.burt@gmail.com>
2014-05-23 15:34:16 -0600

    switched from JSON parser to Oj for faster performance

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -2,10 +2,7 @@ class FeedController < ApplicationController
 
   def index
     feed = Feed.new(current_user)
-    posts_time = Benchmark.realtime do
-      @timeline = feed.posts
-    end
-    Rails.logger.info(("-" * 10) + "POSTS TIME: #{posts_time}")
+    @timeline = feed.posts
     @unauthed_accounts = feed.unauthed_accounts
     @poster_recipient_profile_hash = feed.poster_recipient_profile_hash
     @commenter_profile_hash = feed.commenter_profile_hash

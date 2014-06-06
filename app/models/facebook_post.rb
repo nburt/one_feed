44f059abc2e3c4d784cf4c5cf6bf03ae6a1ced6e
Nathanael Burt <nathanael.burt@gmail.com>
2014-06-06 08:56:27 -0600

    changed if/else statements to case statements

--- app/models/facebook_post.rb
+++ app/models/facebook_post.rb
@@ -6,56 +6,61 @@ class FacebookPost
   end
 
   def to_hash
-    if @post["type"] == "photo"
-      photo_type(@post)
-    elsif @post["type"] == "status"
-      status_type(@post)
-    elsif @post["type"] == "video"
-      video_type(@post)
-    elsif @post["type"] == "link"
-      link_type(@post)
-    else
-      @facebook_post_creator.default_post(@post)
+    case @post["type"]
+      when "photo"
+        photo_type(@post)
+      when "status"
+        status_type(@post)
+      when "video"
+        video_type(@post)
+      when "link"
+        link_type(@post)
+      else
+        @facebook_post_creator.default_post(@post)
     end
   end
 
   private
 
   def link_type(post)
-    if post["status_type"] == "shared_story"
-      @facebook_post_creator.link_shared_story(post)
-    else
-      @facebook_post_creator.default_post(post)
+    case post["status_type"]
+      when "shared_story"
+        @facebook_post_creator.link_shared_story(post)
+      else
+        @facebook_post_creator.default_post(post)
     end
   end
 
   def status_type(post)
-    if post["status_type"] == "wall_post"
-      @facebook_post_creator.wall_post(post)
-    elsif post["status_type"] == "mobile_status_update"
-      @facebook_post_creator.mobile_update(post)
-    else
-      @facebook_post_creator.default_post(post)
+    case post["status_type"]
+      when "wall_post"
+        @facebook_post_creator.wall_post(post)
+      when "mobile_status_update"
+        @facebook_post_creator.mobile_update(post)
+      else
+        @facebook_post_creator.default_post(post)
     end
   end
 
   def photo_type(post)
-    if post["status_type"] == "tagged_in_photo"
-      @facebook_post_creator.tagged_in_photo(post)
-    elsif post["status_type"] == "shared_story"
-      @facebook_post_creator.photo_shared_story(post)
-    elsif post["status_type"] == "added_photos"
-      @facebook_post_creator.added_photos(post)
-    else
-      @facebook_post_creator.photo(post)
+    case post["status_type"]
+      when "tagged_in_photo"
+        @facebook_post_creator.tagged_in_photo(post)
+      when "shared_story"
+        @facebook_post_creator.photo_shared_story(post)
+      when "added_photos"
+        @facebook_post_creator.added_photos(post)
+      else
+        @facebook_post_creator.photo(post)
     end
   end
 
   def video_type(post)
-    if post["status_type"] == "shared_story"
-      @facebook_post_creator.video_shared_story(post)
-    else
-      @facebook_post_creator.default_post(post)
+    case post["status_type"]
+      when "shared_story"
+        @facebook_post_creator.video_shared_story(post)
+      else
+        @facebook_post_creator.default_post(post)
     end
   end
 
